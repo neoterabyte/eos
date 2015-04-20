1c8d384a87ad5b51f47525cdd85554fca355b60c
Neoterabyte <neoterabyte@gmail.com>
2015-04-20 13:15:49 +0100

    Updates

--- like_engine.js
+++ like_engine.js
@@ -124,8 +124,7 @@ function startLikeEngine (agent, timeout, reset_last_access){
 
 						request(options1, function (error1, response1, body1) {
 
-							var headers = JSON.stringify(response1.headers);
-							logger.info("Instagram Remaining Limit for agent: " + agent.user_name + ": " + headers.x-ratelimit-remaining);
+							logger.info("Instagram Remaining Limit for agent: " + agent.user_name + ": " + response1.headers.x-ratelimit-remaining);
 
 							if (error1){
 								errmsg = "Instagram API error: agent: " + agent.user_name + ", error: " + error1;
